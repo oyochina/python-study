705f43058df02bf795ab0d7e3fa7c21b3f5d54d7
oyochina <oyochina@126.com>
2015-02-27 10:17:30 +0800

    I added "modify"in first line Signed-off-by: oyochina <oyochina@126.com>

--- 1.py
+++ 1.py
@@ -1,3 +1,4 @@
+#modify
 import wx
 app = wx.App(False)  # Create a new app, don't redirect stdout/stderr to a window.
 frame = wx.Frame(None, wx.ID_ANY, "Hello World") # A Frame is a top-level window.
